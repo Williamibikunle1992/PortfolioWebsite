c51b00fbfe54269f6aa274bc3468b9aff9147fa3
william ibikunle <w.ibikunle92@gmail.com>
2019-04-12 15:36:07 +0100

    changes was made

--- js/script.js
+++
@@ -1,9 +0,0 @@
-/* jshint esversion:6 */
-
-$(document).ready(function() {
-  $('html, body, *').mousewheel(function(e, delta) {
-    this.scrollLeft -= (delta);
-    e.preventDefault();
-  });
-
-});
